66c16bf371998e12c49920fbe946ac9d0758f7bf
whkim15 <wkim15@vols.utk.edu>
2024-04-29 00:43:40 -0400

    raster

---
+++ pages/7_🛰️_Raster.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import leafmap.foliumap as leafmap
+
+st.set_page_config(layout="wide")
+
+markdown = """
+A Streamlit map template
+<https://github.com/whkim15/map-template>
+"""
+
+st.sidebar.title("About")
+st.sidebar.info(markdown)
+logo = "https://i.imgur.com/UbOXYAU.png"
+st.sidebar.image(logo)
+
+st.title("Split-panel Map")
+
+with st.expander("See source code"):
+    with st.echo():
+        m = leafmap.Map()
+        before = "https://github.com/opengeos/datasets/releases/download/raster/Libya-2023-07-01.tif"
+        after = "https://github.com/opengeos/datasets/releases/download/raster/Libya-2023-09-13.tif"
+        m.split_map(
+            left_layer=before, right_layer=after
+        )
+
+m.to_streamlit(height=700)
